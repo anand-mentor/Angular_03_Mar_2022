d57e5ea204684e86571867bfe0f079f99f87b707
Anand Kulkarni <anand.kulkarni1@zensar.com>
2022-03-08 08:54:34 +0530

    DAY-3

---
+++ shared_code/form-validation/product/product-plain.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'product-plain',
+    templateUrl: './product-plain.component.html'
+})
+export class ProductPlainComponent {
+}
